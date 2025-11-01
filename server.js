cb38fffeb29b4039ecc072016aac437815075d9f
LORD-MEGA MD <ttawmega@gmail.com>
2025-11-02 01:35:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get("/view", async (req, res) => {
 
 // Serve device-logger.html as homepage
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "device-logger.html"));
 });
 
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
